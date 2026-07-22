78c013c96e03bb6872ac633ffd1197c447fc3b2c
wilovy <emiliano@adquiere.co>
2026-07-21 18:18:01 -0600

    feat(headers): update User-Agent string to reflect current browser version

--- php-cli/vendor/phpcfdi/cfdi-sat-scraper/src/Internal/Headers.php
+++ php-cli/vendor/phpcfdi/cfdi-sat-scraper/src/Internal/Headers.php
@@ -26,7 +26,7 @@ class Headers
             'Accept-Charset' => 'utf-8, iso-8859-15;q=0.5',
             'Connection' => 'keep-alive',
             'Referer' => $referer,
-            'User-Agent' => 'Mozilla/5.0 (Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0',
+            'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         ]);
     }
 
